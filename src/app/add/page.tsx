0898a64a5fc4d1cf2ceb189e3e85515aebd7c8ba
yarkincaner <yarkincaner@gmail.com>
2023-08-13 17:40:06 +0300

    feat: add AddPage for admins
    add AddPage to app router and add cloudinary result domain to next.config.js.
    
    AddPage still not exist on navbar but it's reachable through path. Everthing works except uploading images to cloudinary. CORS policy blocks fetch method.

---
+++ src/app/add/page.tsx
@@ -0,0 +1,204 @@
+"use client"
+
+import { useSession } from "next-auth/react"
+import Image from "next/image"
+import { useRouter } from "next/navigation"
+import React, { useState } from "react"
+import styles from "./page.module.css"
+
+type Inputs = {
+	title: string
+	desc: string
+	price: number
+	catSlug: string
+}
+
+type Option = {
+	title: string
+	additionalPrice: number
+}
+
+const AddPage = () => {
+	const { data: session, status } = useSession()
+	const [inputs, setInputs] = useState<Inputs>({
+		title: "",
+		desc: "",
+		price: 0,
+		catSlug: "",
+	})
+
+	const [option, setOption] = useState<Option>({
+		title: "",
+		additionalPrice: 0,
+	})
+
+	const [options, setOptions] = useState<Option[]>([])
+	const [file, setFile] = useState<File>()
+
+	const router = useRouter()
+
+	if (status === "loading") {
+		return <p>Loading...</p>
+	}
+
+	if (status === "unauthenticated" || !session?.user.isAdmin) {
+		router.push("/")
+	}
+
+	const handleChange = (
+		e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+	) => {
+		setInputs((prev) => {
+			return { ...prev, [e.target.name]: e.target.value }
+		})
+	}
+	const changeOption = (e: React.ChangeEvent<HTMLInputElement>) => {
+		setOption((prev) => {
+			return { ...prev, [e.target.name]: e.target.value }
+		})
+	}
+
+	const handleChangeImg = (e: React.ChangeEvent<HTMLInputElement>) => {
+		const target = e.target as HTMLInputElement
+		const item = (target.files as FileList)[0]
+		setFile(item)
+	}
+
+	// TODO: Fix the error: "fetch has been blocked by CORS policy. No 'Access-Control-Allow-Origin' header is present on the requested resource"
+	const upload = async () => {
+		const data = new FormData()
+		data.append("file", file!)
+		data.append("upload_preset", "restaurant")
+
+		const res = await fetch("https://api.cloudinary.com/v1_1/dgpwtxipn/image", {
+			method: "POST",
+			headers: { "Content-Type": "multipart/form-data" },
+			body: data,
+		})
+
+		const resData = await res.json()
+		return resData.url
+	}
+
+	const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault()
+
+		try {
+			const url = await upload()
+			const res = await fetch("http://localhost:3000/api/products", {
+				method: "POST",
+				body: JSON.stringify({
+					img: url,
+					...inputs,
+					options,
+				}),
+			})
+
+			const data = await res.json()
+
+			router.push(`/product/${data.id}`)
+		} catch (err) {
+			console.log(err)
+		}
+	}
+
+	return (
+		<div>
+			<form onSubmit={handleSubmit} className={styles.form}>
+				<h1>Add New Product</h1>
+				<div className={styles.item}>
+					<label htmlFor="file">
+						<Image src="/upload.png" alt="" width={30} height={20} />
+						<span>Upload Image</span>
+					</label>
+					<input
+						type="file"
+						onChange={handleChangeImg}
+						id="file"
+						className={styles.hidden}
+					/>
+				</div>
+				<div className={styles.item}>
+					<label>Title</label>
+					<input
+						type="text"
+						placeholder="Bella Napoli"
+						name="title"
+						onChange={handleChange}
+					/>
+				</div>
+				<div className={styles.item}>
+					<label>Description</label>
+					<textarea
+						rows={3}
+						placeholder="A timeless favorite with a twist, showcasing a thin crust topped with sweet tomatoes, fresh basil and creamy mozzarella."
+						name="desc"
+						onChange={handleChange}
+					/>
+				</div>
+				<div className={styles.item}>
+					<label>Price</label>
+					<input
+						type="number"
+						placeholder="29"
+						name="price"
+						onChange={handleChange}
+					/>
+				</div>
+				<div className={styles.item}>
+					<label>Category</label>
+					<input
+						type="text"
+						placeholder="pizzas"
+						name="catSlug"
+						onChange={handleChange}
+					/>
+				</div>
+				<div className={`${styles.item} ${styles.optionsContainer}`}>
+					<label>Options</label>
+					<div className={styles.item}>
+						<input
+							type="text"
+							placeholder="Title"
+							name="title"
+							onChange={changeOption}
+						/>
+						<input
+							type="number"
+							placeholder="Additional Price"
+							name="additionalPrice"
+							onChange={changeOption}
+						/>
+						<div
+							className={styles.optionButton}
+							onClick={() => setOptions((prev) => [...prev, option])}
+						>
+							Add Option
+						</div>
+					</div>
+					<div>
+						{options.map((opt) => (
+							<div
+								className={styles.option}
+								key={opt.title}
+								onClick={() =>
+									setOptions((prev) =>
+										prev.filter((item) => item.title !== opt.title)
+									)
+								}
+							>
+								<span>{opt.title}</span>
+								<span> (+ ${opt.additionalPrice})</span>
+							</div>
+						))}
+					</div>
+				</div>
+				<div className={styles.item}>
+					<button type="submit">Submit</button>
+				</div>
+			</form>
+		</div>
+	)
+}
+
+export default AddPage
--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -19,3 +19,19 @@ export const GET = async (req: NextRequest) => {
 		return new NextResponse(JSON.stringify({message: "Something went wrong!"}), { status: 500 });
 	}
 }
+
+export const POST = async (req: NextRequest) => {
+  try {
+    const body = await req.json();
+    const product = await prisma.product.create({
+      data: body,
+    });
+    return new NextResponse(JSON.stringify(product), { status: 201 });
+  } catch (err) {
+    console.log(err);
+    return new NextResponse(
+      JSON.stringify({ message: "Something went wrong!" }),
+      { status: 500 }
+    );
+  }
+};
